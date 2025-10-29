99890f81c015aa8261ca49d3e6c941f7b3e87860
Pedro Teixeira <pedro.progteixeira@gmail.com>
2025-10-29 11:56:08 +0000

    feat(app): add state management and search feature using class components

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import CardList from './components/CardList.jsx'
+import App from './App.jsx'
 import 'tachyons'
-import {robots} from './data/robots.js'
+
 
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <CardList robots={robots}/>
+    <App/>
   </StrictMode>,
 )
